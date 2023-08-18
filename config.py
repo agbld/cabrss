ee9d688d691e1be7d04a1dfb89d7e90273ba68dd
agiblida <agiblida@gmail.com>
2023-08-18 11:12:46 +0800

    setup path for paperspace:A100

--- config.py
+++ config.py
@@ -26,6 +26,11 @@ if machine == 'agbld.host':
     ruten_datasets_folder = 'E:/share_disk/Datasets/Ruten/'
     experiments_folder = './Experiments/'
     pretrained_models_folder = 'E:/share_disk/Models/ICL/pretrained_models/'
+if machine == 'paperspace.A100':
+    # pchome_datasets_folder = 'E:/share_disk/Datasets/PChome_datasets/'
+    ruten_datasets_folder = '/mnt/share_disk/Datasets/Ruten/'
+    experiments_folder = './Experiments/'
+    pretrained_models_folder = '/mnt/share_disk/Models/ICL/pretrained_models/'
 if machine == 'your.machine.name':
     pchome_datasets_folder = '/path/to/pchome/datasets/'
     experiments_folder = '/path/to/experiments/'
@@ -60,55 +65,10 @@ ruten_qrels_path = os.path.join(ruten_datasets_folder, 'small_dataset', 'qrels.p
 # CONFIGs
 
 # exp_name = 'ruten-test'
-exp_name = 'exp11_ECom-BERT_xbm_batch-hard-loss_train-sm_intent-based-book-neg-2_valid-on-round0-plus'
-is_test_run = False
-
-if exp_name == 'ruten-test':
-    # train dataset
-    query_item_pairs_path = trace_log_sm_path
-    offline_mining_strategy = {
-        'mine-neg-strategy': 'naive',
-        'neg-num': 2,
-    }
-    mining_neg_result_folder = os.path.join(experiments_folder, exp_name, 
-                                            'mining_neg_result', 
-                                            f'{offline_mining_strategy["mine-neg-strategy"]}_neg_num_{offline_mining_strategy["neg-num"]}')
-    # valid dataset
-    valid_query_path = ruten_test_query_path
-    valid_product_collection_path = ruten_product_collection_sm_path
-    valid_qrels_path = ruten_qrels_path
-    # test dataset
-    test_query_path = ruten_test_query_path
-    test_product_collection_path = ruten_product_collection_sm_path
-    test_qrels_path = ruten_qrels_path
-    # training config                               
-    pretrained_model_path = os.path.join(pretrained_models_folder, 'ecom-bert')
-    loss = 'batch-hard-triplet-loss'
-    epochs = 10
-    batch_size = 64
-    # acceleration (enable only one of them)
-    use_fp16 = False
-    use_amp = True
-    # xbm setting
-    xbm_enable = False
-    xbm_start_iteration = 0
-    xbm_size = batch_size*3
-    # eval & save
-    evaluation_steps = 200
-    save_model_path = experiments_folder + exp_name # which is SentenceTransformer(output_path: str)
-    save_best_model = True # for reference only, its currently using the evaluation dataset to pick the best model
-    checkpoint_path = save_model_path + '/checkpoint'
-    checkpoint_save_steps = 1000    # save checkpoint every 1000 steps
-    checkpoint_save_total_limit = 10    # keep only the last 10 checkpoints
-
-    if is_test_run:
-        exp_name += '_test'
-        query_item_pairs_path = trace_log_tiny_path
-        mining_neg_result_folder = os.path.join(experiments_folder, exp_name, 
-                                                'mining_neg_result', 
-                                                f'{offline_mining_strategy["mine-neg-strategy"]}_neg_num_{offline_mining_strategy["neg-num"]}')
-        save_model_path = experiments_folder + exp_name # which is SentenceTransformer(output_path: str)
-        epochs = 3
+# exp_name = 'exp11_ECom-BERT_xbm_batch-hard-loss_train-sm_intent-based-book-neg-2_valid-on-round0-plus'
+# exp_name = 'ruten-adaption-from-ecom-bert'
+exp_name = 'ruten-adaption-from-ckip-bert'
+is_test_run = True
 
 if exp_name == 'exp11_ECom-BERT_xbm_batch-hard-loss_train-sm_intent-based-book-neg-2_valid-on-round0-plus':
     # TRAIN DATASET
@@ -156,4 +116,99 @@ if exp_name == 'exp11_ECom-BERT_xbm_batch-hard-loss_train-sm_intent-based-book-n
         #                                         f'{offline_mining_strategy["mine-neg-strategy"]}_neg_num_{offline_mining_strategy["neg-num"]}')
         save_model_path = experiments_folder + exp_name # which is SentenceTransformer(output_path: str)
         # epochs = 3
+
+if exp_name == 'ruten-adaption-from-ecom-bert':
+    # train dataset
+    query_item_pairs_path = trace_log_sm_path
+    offline_mining_strategy = {
+        'mine-neg-strategy': 'naive',
+        'neg-num': 2,
+    }
+    mining_neg_result_folder = os.path.join(experiments_folder, exp_name, 
+                                            'mining_neg_result', 
+                                            f'{offline_mining_strategy["mine-neg-strategy"]}_neg_num_{offline_mining_strategy["neg-num"]}')
+    # valid dataset
+    valid_query_path = ruten_test_query_path
+    valid_product_collection_path = ruten_product_collection_sm_path
+    valid_qrels_path = ruten_qrels_path
+    # test dataset
+    test_query_path = ruten_test_query_path
+    test_product_collection_path = ruten_product_collection_sm_path
+    test_qrels_path = ruten_qrels_path
+    # training config                               
+    pretrained_model_path = os.path.join(pretrained_models_folder, 'ecom-bert')
+    loss = 'batch-hard-triplet-loss'
+    epochs = 10
+    batch_size = 64
+    # acceleration (enable only one of them)
+    use_fp16 = False
+    use_amp = True
+    # xbm setting
+    xbm_enable = False
+    xbm_start_iteration = 0
+    xbm_size = batch_size*3
+    # eval & save
+    evaluation_steps = 200
+    save_model_path = experiments_folder + exp_name # which is SentenceTransformer(output_path: str)
+    save_best_model = True # for reference only, its currently using the evaluation dataset to pick the best model
+    checkpoint_path = save_model_path + '/checkpoint'
+    checkpoint_save_steps = 1000    # save checkpoint every 1000 steps
+    checkpoint_save_total_limit = 10    # keep only the last 10 checkpoints
+
+    if is_test_run:
+        exp_name += '_test'
+        query_item_pairs_path = trace_log_tiny_path
+        mining_neg_result_folder = os.path.join(experiments_folder, exp_name, 
+                                                'mining_neg_result', 
+                                                f'{offline_mining_strategy["mine-neg-strategy"]}_neg_num_{offline_mining_strategy["neg-num"]}')
+        save_model_path = experiments_folder + exp_name # which is SentenceTransformer(output_path: str)
+        epochs = 3
+
+if exp_name == 'ruten-adaption-from-ckip-bert':
+    # train dataset
+    query_item_pairs_path = trace_log_sm_path
+    offline_mining_strategy = {
+        'mine-neg-strategy': 'naive',
+        'neg-num': 2,
+    }
+    mining_neg_result_folder = os.path.join(experiments_folder, exp_name, 
+                                            'mining_neg_result', 
+                                            f'{offline_mining_strategy["mine-neg-strategy"]}_neg_num_{offline_mining_strategy["neg-num"]}')
+    # valid dataset
+    valid_query_path = ruten_test_query_path
+    valid_product_collection_path = ruten_product_collection_sm_path
+    valid_qrels_path = ruten_qrels_path
+    # test dataset
+    test_query_path = ruten_test_query_path
+    test_product_collection_path = ruten_product_collection_sm_path
+    test_qrels_path = ruten_qrels_path
+    # training config                               
+    pretrained_model_path = os.path.join(pretrained_models_folder, 'ckiplab-bert-base-chinese')
+    loss = 'batch-hard-triplet-loss'
+    epochs = 10
+    batch_size = 64
+    # acceleration (enable only one of them)
+    use_fp16 = False
+    use_amp = True
+    # xbm setting
+    xbm_enable = False
+    xbm_start_iteration = 0
+    xbm_size = batch_size*3
+    # eval & save
+    evaluation_steps = 200
+    save_model_path = experiments_folder + exp_name # which is SentenceTransformer(output_path: str)
+    save_best_model = True # for reference only, its currently using the evaluation dataset to pick the best model
+    checkpoint_path = save_model_path + '/checkpoint'
+    checkpoint_save_steps = 1000    # save checkpoint every 1000 steps
+    checkpoint_save_total_limit = 10    # keep only the last 10 checkpoints
+
+    if is_test_run:
+        exp_name += '_test'
+        query_item_pairs_path = trace_log_tiny_path
+        mining_neg_result_folder = os.path.join(experiments_folder, exp_name, 
+                                                'mining_neg_result', 
+                                                f'{offline_mining_strategy["mine-neg-strategy"]}_neg_num_{offline_mining_strategy["neg-num"]}')
+        save_model_path = experiments_folder + exp_name # which is SentenceTransformer(output_path: str)
+        epochs = 3
+
 #%%
